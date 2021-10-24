0e74366cdb97ade159c126e307e2a7b9f0534a93
Коган Михаил Александрович <makogan@domclick.ru>
2021-10-24 15:40:35 +0300

    add insertion sort

---
+++ algorithms_ruby/insertion_sort.rb
@@ -0,0 +1,20 @@
+arr = (1..3_000).to_a.shuffle
+
+def insertion_sort(array)
+  for i in 1...(array.length)
+    j = i
+    while j > 0
+      if array[j-1] > array[j]
+        temp = array[j]
+        array[j] = array[j-1]
+        array[j-1] = temp
+      else
+        break
+      end
+
+      j -= 1
+    end
+  end
+
+  array
+end
